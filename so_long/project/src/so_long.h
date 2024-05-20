c80991a811c4c2bcfca4a2f96f53a9ff96c73017
howset <howsetya@googlemail.com>
2024-05-20 14:33:59 +0200

    Makefile works

---
+++ so_long/project/src/so_long.h
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   so_long.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hsetyamu <hsetyamu@student.42berlin.de>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/20 12:36:52 by hsetyamu          #+#    #+#             */
+/*   Updated: 2024/05/20 14:20:12 by hsetyamu         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SO_LONG_H
+# define SO_LONG_H
+
+# include "../lib/minilibx-linux/mlx.h"
+
+typedef struct s_win
+{
+	void	*mlx_ptr;
+	void	*win_ptr;
+	int		height;
+	int		width;
+}		t_win;
+
+typedef struct s_img
+{
+	t_win	win;
+	void	*img_ptr;
+	char	*addr;
+	int		height;
+	int		width;
+	int		bpp;
+	int		endian;
+	int		line_len;
+}		t_img;
+
+#endif
