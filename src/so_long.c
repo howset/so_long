99b7ce1d934f36717f5a4edbb5a313441fdc1735
Howard Setyamukti <hsetyamu@c4b7c1.42berlin.de>
2024-06-07 15:52:46 +0200

    .

--- src/so_long.c
+++ src/so_long.c
@@ -6,17 +6,19 @@
 /*   By: hsetyamu <hsetyamu@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/18 17:28:53 by hsetyamu          #+#    #+#             */
-/*   Updated: 2024/06/07 14:24:40 by hsetyamu         ###   ########.fr       */
+/*   Updated: 2024/06/07 15:02:08 by hsetyamu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../lib/minilibx-linux/mlx.h"
 #include "so_long.h"
 
+
+
 int	main(int argc, char *argv)
 {
 	// check args --> send to error check function, maybe in new file
 	// check map --> together with check args? or not?
 	// initialize window --> in a different file?
-	// 
+	
 }
