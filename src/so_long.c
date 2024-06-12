35d8e4bbe0b8d01fbd379d523b436012db393cc4
howset <howsetya@googlemail.com>
2024-06-12 21:45:06 +0200

    from t590

--- src/so_long.c
+++ src/so_long.c
@@ -6,19 +6,69 @@
 /*   By: hsetyamu <hsetyamu@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/18 17:28:53 by hsetyamu          #+#    #+#             */
-/*   Updated: 2024/06/07 15:02:08 by hsetyamu         ###   ########.fr       */
+/*   Updated: 2024/06/12 18:22:14 by hsetyamu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../lib/minilibx-linux/mlx.h"
 #include "so_long.h"
 
-
-
-int	main(int argc, char *argv)
+/* int	main(int argc, char *argv[])
 {
 	// check args --> send to error check function, maybe in new file
+	if (argc != 2)
+		ft_putchar_fd("Invalid number of arguments.\n", 2);
+	
+	
 	// check map --> together with check args? or not?
 	// initialize window --> in a different file?
 	
+} */
+
+t_img	new_file_img(char * path, t_win window) {
+	t_img image;
+
+	image.win = window;
+	image.img_ptr = mlx_xpm_file_to_image(window.mlx_ptr, path, &image.w, &image.h);
+	if (!image.img_ptr)
+		write(2, "File could not be read\n", 23);
+	else
+		image.addr = mlx_get_data_addr(image.img_ptr, &(image.bpp),
+			&(image.line_len), &(image.endian));
+	return (image);
+}
+
+unsigned int	get_pixel_img(t_img img, int x, int y)
+{
+	return (*(unsigned int *)((img.addr
+			+ (pixel_y * img.line_len) + (pixel_x * img.bpp / 8))));
+}
+
+int main(void)
+{
+ 	t_win	tutorial;
+
+	tutorial = new_window(600, 500, "animations");
+	if (!tutorial.win_ptr)
+		return (2);
+	image = new_img(600, 500, tutorial);
+
+	{
+		/* Put white pixel */
+		put_pixel_img(image, 150, 150, 0x00FFFFFF);
+		mlx_put_image_to_window (image.win.mlx_ptr, image.win.win_ptr, image.img_ptr, 0, 0);
+		destroy_image(image);
+	}
+	{
+		/* Put image */
+		t_img	tloz_img;
+		tloz_img = new_file_img("assets/link.xpm", tutorial);
+		if (!tloz_img.img_ptr)
+			return (1);
+		mlx_put_image_to_window (tloz_img.win.mlx_ptr, tloz_img.win.win_ptr, tloz_img.img_ptr, 0, 0);	
+		destroy_image(tloz_img);
+	}
+	mlx_loop(tutorial.mlx_ptr);
+	destroy_window(tutorial);
+	return (0);
 }
--- src/so_long.h
+++ src/so_long.h
@@ -6,7 +6,7 @@
 /*   By: hsetyamu <hsetyamu@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/20 12:36:52 by hsetyamu          #+#    #+#             */
-/*   Updated: 2024/06/04 15:11:51 by hsetyamu         ###   ########.fr       */
+/*   Updated: 2024/06/12 18:23:21 by hsetyamu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,12 +19,23 @@
 # include <X11/keysym.h>
 # include "../lib/minilibx-linux/mlx.h"
 
-typedef struct s_data
+/* typedef struct s_data
 {
 	void		*mlx_ptr; // MLX pointer
 	void		*win_ptr; // MLX window pointer
 	void		*textures[5]; // MLX image pointers (on the stack)
 	//t_map		*map; // Map pointer (contains map details - preferably kept on the stack)
-}	t_data;
+}	t_data; */
+
+typedef struct s_win {
+	void	*mlx_ptr;
+	void	*win_ptr;
+	int	height;
+	int	width;
+	t_rect	bg;
+}		t_win;
+
+
+}		t_circle;
 
 #endif
